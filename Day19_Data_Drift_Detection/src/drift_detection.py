16daee3bc8f3905491754958ed25fc736fb1ed60
Yash krishna desai <153525180+Yash5847@users.noreply.github.com>
2026-01-15 18:33:46 +0530

    Update drift_detection.py

--- Day19_Data_Drift_Detection/src/drift_detection.py
+++ Day19_Data_Drift_Detection/src/drift_detection.py
@@ -2,18 +2,15 @@ import pandas as pd
 from evidently.report import Report
 from evidently.metric_preset import DataDriftPreset
 
-# Load datasets
 reference_data = pd.read_csv("data/reference_data.csv")
 current_data = pd.read_csv("data/current_data.csv")
 
-# Create drift report
 drift_report = Report(
     metrics=[
         DataDriftPreset()
     ]
 )
 
-# Run drift detection
 drift_report.run(
     reference_data=reference_data,
     current_data=current_data
@@ -23,3 +20,4 @@ drift_report.run(
 drift_report.save_html("reports/drift_report.html")
 
 print("✅ Data Drift Report Generated Successfully!")
+
